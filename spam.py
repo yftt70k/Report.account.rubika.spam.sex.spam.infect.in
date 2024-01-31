fabdcec4027a79bc9c7157a0e1e1aab4b57369eb
yftt70k <156324032+yftt70k@users.noreply.github.com>
2024-01-31 18:02:50 +0330

    spam.py

---
+++ spam.py
@@ -0,0 +1,23 @@
+#Report.account.rubika.spam.sex.spam.infect.in
+
+Filtering.rubik.spam(@...)
+
+print("Рассылая спам-сообщения, этот пользователь вызывает сбой Рубика") 
+
+((Filter.py/account-spam-Report([Filter-account<@3.6.4))
+
+print("Spam account filter and against Rubika's rules")
+
+id account spam(@...)
+
+(Filter.py/account-spam-Report([Filter-account<@3.6.4)
+
+ print("spam.py")
+
+#BAG.spam.account("3.4.6.spam.4.2.1.5.7.8.9.6.4.0.1.7.0.1.0.8.4.1.4.5.0.0.9.4.3.6.8.9.0.9.2.4.2.5.9.0.0.0.2.5.8.1.5.2.8.3.9.4.0.5")
+https://spam.ir/BAG
+
+spam(Этот аккаунт полон нарушений и действует против правил Рубицы, его следует немедленно заблокировать)
+
+print("Report account khalaf ghavanin rubika")
+
