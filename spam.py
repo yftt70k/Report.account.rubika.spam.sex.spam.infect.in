c839b00e21d00ad39427c47452086104e4323bf3
yftt70k <156324032+yftt70k@users.noreply.github.com>
2024-01-31 18:18:57 +0330

    spam.py

--- spam.py
+++ spam.py
@@ -1,7 +1,11 @@
 #Report.account.rubika.spam.sex.spam.infect.in
 
-Filtering.rubik.spam(@...)
+https://www.ppbtube.info
+Filtering.rubika.sex.spam(@...)
 
+(https://Filter.Filter.Filter1Оскорбительные национальности, этничец и убеждений сообществ($®flpom/ftggy&)("https://s8.uupload.ir/files/screenshot_۲۰۲۳۰۵۱۶_۱۶۲۵۳۱_v8p.jpg"($®Report/spam)(Filter.Rubika.spam)($®spam/Report)
+
+ (https://7.0.9.0.0.0.0.0.14.0.0.0.14.0.0.0.1.1044921875.0.0.2.0.0.8955078125.2.0.2.0.0.0.0.0.0.0.14.0.0.0.14.0.0)
 print("Рассылая спам-сообщения, этот пользователь вызывает сбой Рубика") 
 
 ((Filter.py/account-spam-Report([Filter-account<@3.6.4))
@@ -20,4 +24,9 @@ https://spam.ir/BAG
 spam(Этот аккаунт полон нарушений и действует против правил Рубицы, его следует немедленно заблокировать)
 
 print("Report account khalaf ghavanin rubika")
-
+                                                                                                           
+"m9=/1.5c-4.14-0-7.5-3.36-7.5 7.5s3.36 7.5 7.5 7.5-7.5-3.36-7.5-7.5-3.36-7.5-7.5-7.5zm0 13.5c-3.3075 0-6-2.6925-6-6s2.6925-6 6-6 6 2.6925 6 6-2.6925 6-6-6zm-1.5-4.35-1.95-1.95-1.05 1.05 3 3 6-6-1.05-1.05z" fill"
+                                                                                                           
+#Spam<Report.py\M12.3-2.9c.1.1.2.1.3.2.7.6-1.3-1.1-2-1.7.3.2.6.4.9.4.9.1-1.7.2'rf/q.C.rd/s.d.if</#c=b0Y0a2cafbaf668e282d2dc02a1fe2a7>if-name=="@...":(Report.rubika.ir/Этот аккаунт полон нарушений и действует против правил Рубицы, его следует немедленно заблокировать.)@SupportBot<Report_Check>Spam%100                                                                              
+Filter account sapma(@...)
+print("@SupportBot Filter kun account spam")
